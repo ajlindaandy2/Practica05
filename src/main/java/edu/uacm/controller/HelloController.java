5724720877fc810ed39752a5f5d70bf9f8ba9ebf
Profesor <Profesor@A-005-4149>
2016-03-30 08:56:25 -0600

    primer commit

---
+++ src/main/java/edu/uacm/controller/HelloController.java
@@ -0,0 +1,21 @@
+package edu.uacm.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	
+	@RequestMapping
+	public String index(){
+		return "Saludos desde spring boot";
+		
+		
+	}
+	
+	
+	
+	
+
+}
